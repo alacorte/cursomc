17c972a84a03e2eb6da886b7351793d176edf951
Andre Leonardo de La Corte <alacorte@gmail.com>
2019-11-16 14:14:28 -0400

    Repository

--- src/main/java/com/andre/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/andre/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,27 @@
 package com.andre.cursomc.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.andre.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias" )
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informatica");
+		Categoria cat2 = new Categoria(2,"Escritorio");
+		
+		List<Categoria> lista = Arrays.asList(cat1, cat2);
+		
+		return lista;
 	}
 
 }
